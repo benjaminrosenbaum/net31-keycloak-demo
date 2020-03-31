fc6a2ec8543bb2a251eaf53378a93351ad1dda63
Ben Rosenbaum <ben.rosenbaum@itsystems.ch>
2020-03-31 09:20:26 +0200

    fixed directory structure

--- launch.bat
+++ launch.bat
@@ -1,10 +1,14 @@
-cd KeycloakDemoAPI 
+cd SampleNetCoreAngularKeycloak/
 start dotnet run
-cd ../../SampleNetCoreAngularKeycloak/
+start C:/Users/ben.rosenbaum/tools/ngrok/ngrok.exe http -subdomain keycloak-demo     http://localhost:5000
+
+cd ../KeycloakDemoAPI 
 start dotnet run
-cd ../net31-keycloak-demo/reverse
-start C:/Users/ben.rosenbaum/tools/ngrok/ngrok.exe http -subdomain keycloak-demo-ui  http://localhost:5000
-start C:/Users/ben.rosenbaum/tools/ngrok/ngrok.exe http -subdomain keycloak-demo-api http://localhost:26060
+
+cd ../reverse
+REM start C:/Users/ben.rosenbaum/tools/ngrok/ngrok.exe http -subdomain keycloak-demo-ui  http://localhost:5000
+REM start C:/Users/ben.rosenbaum/tools/ngrok/ngrok.exe http -subdomain keycloak-demo-api http://localhost:26060
 start docker-compose up --build
-start C:/Users/ben.rosenbaum/tools/ngrok/ngrok.exe http -subdomain keycloak-demo     http://localhost
+REM start C:/Users/ben.rosenbaum/tools/ngrok/ngrok.exe http -subdomain keycloak-demo     http://localhost
+
 cd ..
