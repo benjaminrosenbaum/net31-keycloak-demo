9dcd6acc0ec25522a981287336bfd9b907be61a1
Ben Rosenbaum <ben.rosenbaum@itsystems.ch>
2020-04-06 14:49:02 +0200

    added CORS hosts for webpart demo, turned on logging

--- KeycloakDemoAPI/Startup.cs
+++ KeycloakDemoAPI/Startup.cs
@@ -60,12 +60,13 @@ namespace KeycloakDemoAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // app.UseMiddleware<CorsInspectionMiddleware>();
+            app.UseMiddleware<CorsInspectionMiddleware>();
             app.UseCors(//"AllowAll"
                 options => options.WithOrigins("http://keycloak-demo.ngrok.io",
                     "https://keycloak-demo.ngrok.io", "http://localhost:5000",
                     "https://keycloak-demo-ui.ngrok.io", "http://keycloak-demo-ui.ngrok.io",
-                    "https://keycloak-demo-api.ngrok.io", "http://keycloak-demo-api.ngrok.io").AllowAnyMethod()
+                    "https://keycloak-demo-api.ngrok.io", "http://keycloak-demo-api.ngrok.io",
+                    "https://localhost:4321", "https://intranet-dev-usz.virtualcorp.ch/").AllowAnyMethod()
                 // options.AllowAnyOrigin().AllowAnyMethod()
                 .WithHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization")
                 .WithExposedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization").AllowCredentials()
